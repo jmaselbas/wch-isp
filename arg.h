444e7e134943e04ba807e1f6629bb1b15dd3c6d1
Jules Maselbas <jmaselbas@zdiv.net>
2023-02-03 10:51:16 +0100

    Fix warning in arg.h

--- arg.h
+++ arg.h
@@ -7,11 +7,11 @@ extern char *argv0;
 
 #define ARGBEGIN {char *_arg, **_argp, **_args;				\
 	for (argv0 = *argv++, argc--, _args = _argp = argv;		\
-	     _arg = *_argp;		/* while != NULL */		\
+	     (_arg = *_argp) != NULL;	/* while != NULL */		\
 	     *_argp ? _argp++ : 0)	/* inc only if _argp != NULL */	\
 		if (*_arg == '-' && _arg[1] == '-' && _arg[2] == '\0')	\
 			for (argc--, _argp++;	/* skip the '--' arg */	\
-			     _arg = *_argp;				\
+			     (_arg = *_argp) != NULL;			\
 			     *_argp ? _argp++ : 0)			\
 				*(_args++) = _arg; /* copy all args */	\
 		else if (*_arg == '-' && _arg[1] != '-' && _arg[1] != '\0') \
