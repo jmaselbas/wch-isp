db4fc547b4855462f59394f9ec90800a57aee450
COKPOWEHEU <COKPOWEHEU@yandex.ru>
2023-11-19 21:30:16 +0300

    Add reading .bin and .hex

--- main.c
+++ main.c
@@ -62,7 +62,7 @@ static void isp_cmd_isp_key(isp_dev_t dev, size_t len, uint8_t *key, uint8_t *su
 //exit (if 'reason' == 1 then reset)
 static void isp_cmd_isp_end(isp_dev_t dev, uint8_t reason);
 //
-static void isp_cmd_erase(isp_dev_t dev, uint32_t sectors);
+static char isp_cmd_erase(isp_dev_t dev, uint32_t sectors);
 //
 static uint16_t isp_cmd_program(isp_dev_t dev, uint32_t addr, uint16_t len, uint8_t data[]);
 //
@@ -73,7 +73,11 @@ static uint16_t isp_cmd_verify(isp_dev_t dev, uint32_t addr, uint16_t len, uint8
 static void dev_readinfo(isp_dev_t dev); //read id, type, bootloader_ver and uid
 
 
-
+////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////
+//     ISP commands
+////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////
 static void isp_cmd_identify(isp_dev_t dev){
   if(dev->port == NULL){fprintf(stderr, "isp_cmd_identify: wch_if is NULL\n"); return;}
   uint8_t buf[] = "\0\0MCU ISP & WCH.CN";
@@ -109,7 +113,13 @@ static size_t isp_cmd_read_config(isp_dev_t dev, uint16_t cfgmask, size_t len, u
 }
 
 static void isp_cmd_write_config(isp_dev_t dev, uint16_t cfgmask, size_t len, uint8_t data[]){
-#warning TODO
+  uint8_t buf[64];
+  buf[0] = (cfgmask >> 0) & 0xFF;
+  buf[1] = (cfgmask >> 8) & 0xFF;
+  if(len > dev->port->maxdatasize-3)len = dev->port->maxdatasize-3;
+  memcpy(&buf[2], data, len);
+  dev->port->send(dev->port, CMD_WRITE_CONFIG, len+2, buf);
+  dev->port->recv(dev->port, CMD_WRITE_CONFIG, 2, buf);
 }
 
 static void isp_cmd_isp_key(isp_dev_t dev, size_t len, uint8_t *key, uint8_t *sum){
@@ -140,12 +150,45 @@ static void isp_cmd_isp_end(isp_dev_t dev, uint8_t reason){
   dev->port->recv(dev->port, CMD_ISP_END, sizeof(buf), buf);
 }
 
-static void isp_cmd_erase(isp_dev_t dev, uint32_t sectors){
-  #warning TODO
+static char isp_cmd_erase(isp_dev_t dev, uint32_t sectors){
+  uint8_t buf[64];
+  buf[0] = (sectors >>  0) & 0xff;
+  buf[1] = (sectors >>  8) & 0xff;
+  buf[2] = (sectors >> 16) & 0xff;
+  buf[3] = (sectors >> 24) & 0xff;
+  
+  dev->port->send(dev->port, CMD_ERASE, 4, buf);
+  dev->port->recv(dev->port, CMD_ERASE, 2, buf);
+  
+  if(buf[0] != 0 || buf[1] != 0){
+    fprintf(stderr, "isp_cmd_erase: failed: %.2X %.2X\n", buf[0], buf[1]);
+    return 0;
+  }
+  return 1;
 }
 
 static uint16_t isp_cmd_program(isp_dev_t dev, uint32_t addr, uint16_t len, uint8_t data[]){
-  #warning TODO
+  uint8_t buf[64];
+  if(len > dev->port->maxdatasize-5){
+    fprintf(stderr, "isp_cmd_program: data boot big, %d (max=%d)\n", len, dev->port->maxdatasize-5);
+    return 0;
+  }
+  buf[0] = (addr >>  0) & 0xff;
+  buf[1] = (addr >>  8) & 0xff;
+  buf[2] = (addr >> 16) & 0xff;
+  buf[3] = (addr >> 24) & 0xff;
+  buf[4] = 0; //padding (ignored by bootloader)
+  
+  for(int i=0; i<len; i++)buf[i+5] = data[i] ^ dev->xor_key[i % 8];
+  
+  dev->port->send(dev->port, CMD_PROGRAM, len+5, buf);
+  dev->port->recv(dev->port, CMD_PROGRAM, 2, buf);
+  
+  if(buf[0] != 0 || buf[1] != 0){
+    fprintf(stderr, "isp_cmd_program: failed at address 0x%.8X\n", addr);
+    return 0;
+  }
+  return len;
 }
 
 static uint16_t isp_cmd_verify(isp_dev_t dev, uint32_t addr, uint16_t len, uint8_t data[]){
@@ -184,7 +227,11 @@ static void dev_readinfo(isp_dev_t dev){
   memcpy(dev->uid, &buf[4], 8);
 }
 
-
+////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////
+//   Read file
+////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////
 
 static void file_read_bin(const char name[], size_t *sz, uint8_t **data){
   int res;
@@ -206,30 +253,144 @@ static void file_read_bin(const char name[], size_t *sz, uint8_t **data){
   *data = buf;
 }
 
-static char verify(isp_dev_t dev, const char name[]){
-  uint8_t *data = NULL;
-  size_t data_size;
-  uint32_t addr = 0;
+static void file_read_hex(const char name[], size_t *sz, uint8_t **data){
+  size_t size = 0;
+  uint8_t *buf = NULL;
+  int res;
+  size_t maxaddr = 0;
+  int line = 0;
+  uint8_t linesize;
+  uint16_t lineaddr;
+  uint16_t newaddr;
+  uint8_t linecode;
+  uint8_t linedata;
+  uint8_t lrc;
+  FILE *pf = fopen(name, "rt");
+  if(pf == NULL){fprintf(stderr, "file_read_hex, open [%s]: %s\n", name, strerror(errno)); return;}
+  while(1){
+    line++;
+    res = fscanf(pf, ":%2hhX%4hX%2hhX", &linesize, &lineaddr, &linecode);
+    if(res != 3)break;
+    if(linecode != 0)continue;
+    lrc = linesize;
+    lrc += (lineaddr >> 0) & 0xFF;
+    lrc += (lineaddr >> 8) & 0xFF;
+    newaddr = lineaddr + linesize;
+    //realloc
+    if(newaddr > size)size = newaddr;
+    if(newaddr > maxaddr){
+      size_t maxaddr_prev = maxaddr;
+      uint8_t *buf_prev = buf;
+      maxaddr = (newaddr + 1023) &~1023; //round up to 1k
+      buf = realloc(buf_prev, maxaddr);
+      if(buf == NULL){
+        if(buf_prev)free(buf_prev); 
+        *sz = 0;
+        *data = NULL;
+        return;
+      }
+      memset(&buf[maxaddr_prev], 0, (maxaddr - maxaddr_prev));
+    }
+    //read data
+    for(int i=0; i<linesize; i++){
+      res = fscanf(pf, "%2hhX", &linedata);
+      if(res != 1){
+        fprintf(stderr, "file_read_hex: file format error in line %i\n", line);
+        if(buf)free(buf);
+        *sz = 0; *data = NULL; fclose(pf);
+        return;
+      }
+      buf[lineaddr + i] = linedata;
+      lrc += linedata;
+    }
+    
+    res = fscanf(pf, "%2hhX\n", &linedata);
+    if(res != 1){
+      fprintf(stderr, "file_read_hex: file format error in line %i\n", line);
+      if(buf)free(buf);
+      *sz = 0; *data = NULL; fclose(pf);
+      return;
+    }
+    lrc += linedata;
+    if(lrc != 0){
+      fprintf(stderr, "file_read_hex: LRC error in line %i\n", line);
+      if(buf)free(buf);
+      *sz = 0; *data = NULL; fclose(pf);
+      return;
+    }
+  }
+  fclose(pf);
+  *sz = size;
+  *data = buf;
+}
+
+#ifdef _MSC_VER //not #if defined(_WIN32) || defined(_WIN64) because we have strncasecmp in mingw
+  #define strcasecmp _stricmp
+#endif
+
+static void file_read(const char name[], size_t *sz, uint8_t **data){
+  size_t len = strlen(name);
+  if(len < 4){fprintf(stderr, "Input file must me *.hex or *.bin\n"); *sz=0; *data=NULL; return;}
+  if(strcasecmp(&name[len-4], ".hex")==0){
+    file_read_hex(name, sz, data);
+  }else if(strcasecmp(&name[len-4], ".bin")==0){
+    file_read_bin(name, sz, data);
+  }else{
+    fprintf(stderr, "Input file must me *.hex or *.bin\n");
+    *sz=0; *data=NULL;
+    return;
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////
+//   High-level functions
+////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+static char do_verify(isp_dev_t dev, uint32_t addr, size_t size, uint8_t data[]){
   size_t count = 0;
   uint16_t res = 0xFFFF;
   uint16_t maxdata = dev->port->maxdatasize - 5;
-  file_read_bin(name, &data_size, &data);
-  if(data == NULL)return 1;
-  while(data_size){
-    count = data_size;
+  while(size){
+    count = size;
     if(count > maxdata)count = maxdata;
     res = isp_cmd_verify(dev, addr, count, &data[addr]);
     if(res != count)break;
     addr += count;
-    data_size -= count;
+    size -= count;
     //TODO: progressbar
   }
   free(data);
   if(res == count){printf("Verify: OK\n"); return 0;}else return 2;
 }
 
+static char do_program(isp_dev_t dev, uint32_t addr, size_t size, uint8_t data[]){
+  size_t count = 0;
+  uint16_t res = 0xFFFF;
+  uint16_t maxdata = dev->port->maxdatasize - 5;
+  while(size){
+    count = size;
+    if(count > maxdata)count = maxdata;
+    res = isp_cmd_program(dev, addr, count, &data[addr]);
+    if(res != count)break;
+    addr += count;
+    size -= count;
+    //TODO: progressbar
+  }
+  free(data);
+  if(res == count){printf("Program: OK\n"); return 0;}else return 2;
+}
 
-__attribute__((unused))static char dev_match(wch_if_t self){
+static char do_erase(isp_dev_t dev, size_t size){
+  size = (size + 1023) / 1024;
+  if(size < 8)size = 8;
+  return isp_cmd_erase(dev, size);
+}
+
+
+
+static char dev_match(wch_if_t self){
   isp_dev dev = {.id = 0, .type = 0};
   dev.port = self;
   isp_cmd_identify(&dev);
@@ -255,7 +416,7 @@ int main(){
     fprintf(stderr, "Error opening port / device not found\n");
     return -1;
   }
-  wch_if_set_debug(dev.port, dbg);
+  //wch_if_set_debug(dev.port, dbg);
   
   isp_cmd_identify(&dev);
   dev_readinfo(&dev);
@@ -265,21 +426,12 @@ int main(){
   for(int i=0; i<7; i++)printf("%.2X ", dev.uid[i]);
   printf("%.2X]\n", dev.uid[7]);
   
-  uint8_t isp_key[] = {0xFD, 0x6E, 0xBC, 0xE0, 0x77, 0x1C, 0xA0, 0xAC, 0xF1, 0x51, 0x54, 0xD5, 0xE1, 0x7F, 0x12, 0x04, 0x5F, 0x2C, 0xD8, 0x8C, 0x7C, 0xF6, 0x5D, 0x95, 0xD4, 0xD4, 0x4A, 0x79, 0x67, 0x9E, 0xA7};
+  uint8_t isp_key[30] = {0};
   isp_cmd_isp_key(&dev, sizeof(isp_key), isp_key, NULL);
   
-  /*uint8_t *fil = NULL;
-  size_t fil_len;
-  file_read_bin("test/blink.bin", &fil_len, &fil);
-  if(fil != NULL){
-    uint16_t res = isp_cmd_verify(&dev, 0, 32, fil);
-    printf("verify: %d\n", res);
   
-    free(fil);
-  }else{
-    printf("err\n");
-  }*/
-  verify(&dev, "test/blink.bin");
+  //program(&dev, 1024, "test/blink.bin");
+  //verify(&dev, 1024, "test/blink.bin");
   
-  if(dev.port)dev.port->close(&dev.port);
+  wch_if_close(&dev.port);
 }
