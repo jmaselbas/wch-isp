89307a478404a5162ca2b534ad35d72d11384250
Alexandr <gisi@sunwinder.ru>
2024-01-07 00:51:23 +0300

    Patch flash sector size

--- devices.h
+++ devices.h
@@ -87,7 +87,7 @@ const struct db devices[] = {
 		{ 0x3f, "CH32V103x6x6", SZ_32K },
 		{ /* sentinel */ } }
 	},
-	{ .type = 0x17, .flash_sector_size = 4096,
+	{ .type = 0x17, .flash_sector_size = 1024,
 	  .name = "CH32V30x",
 	  .flash_cfg = &(const struct flash_cfg[4]){
 		  [0] = { SZ_192K, SZ_192K },
